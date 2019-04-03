a8f041ef7b19fa3f5c66edcee78f275f09d83f20
kevin-CodeCrew <kevin@code-crew.org>
2019-04-02 21:53:50 -0500

    Map madness

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,20 @@ const Pot_Holes =require('../Models/PotHoleHit');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  Pot_Holes.find({}, function (err, ph_recs) { //Use the findID method on the data model to search DB
+  Pot_Holes.get({}, function (err, ph_recs)
+  { //Use the find method on the data model to search DB
     if (err) {
       throw err; // If we get an error then bail
     }
-    console.log("Got records " + ph_recs.length);
+   console.log(`Got records ${ph_recs.length}`);
+    ph_recs.map(function(x){
+      console.log(x);
+    });
     res.render('google_maps', {
       google_map_api_key: 'AIzaSyAcQjbN0Wx4DzaqxJTiyOl5ZgaCvGQk4yI',
       pot_holes: ph_recs})
-  });
+  })
 
 });
+
 module.exports = router;
