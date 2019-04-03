f36db6816ca7457d4fd5e764b97653d6c4f90dca
kevin-CodeCrew <kevin@code-crew.org>
2019-04-02 22:21:03 -0500

    Crap

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,17 @@ const Pot_Holes =require('../Models/PotHoleHit');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  Pot_Holes.get({}, function (err, ph_recs)
-  { //Use the find method on the data model to search DB
-    if (err) {
-      throw err; // If we get an error then bail
-    }
-   console.log(`Got records ${ph_recs.length}`);
-    ph_recs.map(function(x){
-      console.log(x);
-    });
+  // Pot_Holes.find({}, function (err, ph_recs)
+  // { //Use the find method on the data model to search DB
+  //   if (err) {
+  //     throw err; // If we get an error then bail
+  //   }
+  //  console.log("Got records " + ph_recs.length);
     res.render('google_maps', {
       google_map_api_key: 'AIzaSyAcQjbN0Wx4DzaqxJTiyOl5ZgaCvGQk4yI',
-      pot_holes: ph_recs})
+      pot_holes: null})
   })
 
-});
+;
 
 module.exports = router;
