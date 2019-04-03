2c0e13679f868f268483ba4dcf587befcfd88ee1
kevin-CodeCrew <kevin@code-crew.org>
2019-04-03 10:13:59 -0500

    Realtime maps

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,41 @@
 var express = require('express');
 var router = express.Router();
-const Pot_Holes =require('../Models/PotHoleHit');
+const Pot_Holes = require('../Models/PotHoleHit');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  // Pot_Holes.find({}, function (err, ph_recs)
-  // { //Use the find method on the data model to search DB
-  //   if (err) {
-  //     throw err; // If we get an error then bail
-  //   }
-  //  console.log("Got records " + ph_recs.length);
-    res.render('google_maps', {
-      google_map_api_key: 'AIzaSyAcQjbN0Wx4DzaqxJTiyOl5ZgaCvGQk4yI',
-      pot_holes: null})
-  })
+    var dataFilter = {
+        __v: false,
+        _id: false,
+        date: false
+    };
 
-;
+    Pot_Holes.find({}, dataFilter,function(err, ph_recs) { //Use the find method on the data model to search DB
+        if (err) {
+            throw err; // If we get an error then bail
+        } else {
+
+
+            console.log("Got records " + ph_recs.length);
+            // var phs = ph_recs.map(function (ph) {
+            //     return {
+            //         "date": "'"+ph.date.toDateString()+"'",
+            //         "x": ph.x,
+            //         "y": ph.y,
+            //         "z": ph.z,
+            //         "latitude": ph.lat,
+            //         "longitude": ph.long
+            //     };
+            // });
+
+            console.log("*" + ph_recs);
+            res.render('google_maps', {
+                pot_holes: ph_recs,
+                google_map_api_key: 'AIzaSyAcQjbN0Wx4DzaqxJTiyOl5ZgaCvGQk4yI'
+            })
+        }
+    });
+});
 
 module.exports = router;
