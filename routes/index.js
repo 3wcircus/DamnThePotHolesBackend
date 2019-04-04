d515516e3aac3f9902f41af38bc995fb8af55e26
kevin-CodeCrew <kevin@code-crew.org>
2019-04-03 22:24:32 -0500

    Screen up

--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,22 @@ var express = require('express');
 var router = express.Router();
 const Pot_Holes = require('../Models/PotHoleHit');
 
-/* GET home page. */
+/* GET landing page. */
 router.get('/', function (req, res, next) {
 
+    // TODO:Figure out how not to exclude the dat field
     var dataFilter = {
         __v: false,
         _id: false,
         date: false
     };
 
+    // Pull hits from remote Mongo instance
     Pot_Holes.find({}, dataFilter,function(err, ph_recs) { //Use the find method on the data model to search DB
         if (err) {
             throw err; // If we get an error then bail
         } else {
-
-
-            console.log("Got records " + ph_recs.length);
-            // var phs = ph_recs.map(function (ph) {
-            //     return {
-            //         "date": "'"+ph.date.toDateString()+"'",
-            //         "x": ph.x,
-            //         "y": ph.y,
-            //         "z": ph.z,
-            //         "latitude": ph.lat,
-            //         "longitude": ph.long
-            //     };
-            // });
-
-            console.log("*" + ph_recs);
+            // No exception, so inject hits and render
             res.render('google_maps', {
                 pot_holes: ph_recs,
                 google_map_api_key: 'AIzaSyAcQjbN0Wx4DzaqxJTiyOl5ZgaCvGQk4yI'
