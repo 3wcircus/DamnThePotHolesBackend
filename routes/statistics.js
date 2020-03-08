8502d7f7df3cac19770504c50d637b956a0b677d
Andie <andrewrtubbs@gmail.com>
2020-03-07 20:31:19 -0600

    Updated statistics 4

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch')
 router.route('/').get((req,res) => {
-    res.render('statistics', {
-        title: 'Statistics'
-    });
+    var nClosedPotholes;
+    let settings = { method: "Get" };
+    fetch('https://data.memphistn.gov/resource/2244-gnrp.json?category=Maintenance-Potholes', settings )
+    .then((res) => {
+        return res.json();
+    })
+    .then((json) => {
+        nClosedPotholes = json.length;
+        res.render('statistics', {
+            title: 'Statistics',
+            nClosedPotholes: nClosedPotholes
+        });
+    
+        return json;
+    })
+
 })
 module.exports = router;
