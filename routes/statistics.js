9555cf7f52469a89d63064b5c0d0a98a7295d9bb
Andie <andrewrtubbs@gmail.com>
2020-03-07 21:20:45 -0600

    Statistics update 5

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch')
+// router.route('/').get((req,res) => {
+//     // var nClosedPotholes;
+//     // let settings = { method: "Get" };
+//     // fetch('https://data.memphistn.gov/resource/2244-gnrp.json?category=Maintenance-Potholes', settings )
+//     // .then((res) => {
+//     //     return res.json();
+//     // })
+//     // .then((json) => {
+//     //     nClosedPotholes = json.length;
+//     //     res.render('statistics', {
+//     //         title: 'Statistics',
+//     //         nClosedPotholes: nClosedPotholes
+//     //     });
+    
+//     //     return json;
+//     // })
+// })
 router.route('/').get((req,res) => {
-    var nClosedPotholes;
-    let settings = { method: "Get" };
-    fetch('https://data.memphistn.gov/resource/2244-gnrp.json?category=Maintenance-Potholes', settings )
+    var nOpenPotholes;
+    let settings2 = { method: "Get" };
+    fetch('https://data.memphistn.gov/resource/aiee-9zqu.json?category=Maintenance-Potholes', settings2 )
     .then((res) => {
         return res.json();
     })
     .then((json) => {
-        nClosedPotholes = json.length;
+        nOpenPotholes = json.length;
         res.render('statistics', {
             title: 'Statistics',
-            nClosedPotholes: nClosedPotholes
+            nOpenPotholes: nOpenPotholes
         });
-    
+
         return json;
     })
-
 })
 module.exports = router;
